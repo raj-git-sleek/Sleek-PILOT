68724df4c4ecd1f7c59ff9e4015c6f27d279baec
Kh Raj Rahman (Admin - Sleek Apparels) <sleekapparelsltd@gmail.com>
2025-10-23 02:50:15 +0000

    feat: replace wordmark with icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,20 +4,23 @@ export const Icons = {
   logo: (props: SVGProps<SVGSVGElement>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 20"
-      fill="currentColor"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <text
-        x="50"
-        y="15"
-        fontFamily="Verdana"
-        fontSize="18"
-        textAnchor="middle"
-        fill="currentColor"
-      >
-        Sleek Memory
-      </text>
+      <path d="M9.5 2A2.5 2.5 0 0 1 12 4.5v0A2.5 2.5 0 0 1 9.5 7h0A2.5 2.5 0 0 1 7 4.5v0A2.5 2.5 0 0 1 9.5 2Z" />
+      <path d="M14.5 2A2.5 2.5 0 0 1 17 4.5v0A2.5 2.5 0 0 1 14.5 7h0A2.5 2.5 0 0 1 12 4.5v0A2.5 2.5 0 0 1 14.5 2Z" />
+      <path d="M12 12a2.5 2.5 0 0 1 2.5 2.5v0a2.5 2.5 0 0 1-5 0v0A2.5 2.5 0 0 1 12 12Z" />
+      <path d="M9.5 22a2.5 2.5 0 0 1-2.5-2.5v0a2.5 2.5 0 0 1 5 0v0A2.5 2.5 0 0 1 9.5 22Z" />
+      <path d="M14.5 22a2.5 2.5 0 0 1-2.5-2.5v0a2.5 2.5 0 0 1 5 0v0A2.5 2.5 0 0 1 14.5 22Z" />
+      <path d="M17 14.5a2.5 2.5 0 0 1-2.5-2.5v0a2.5 2.5 0 0 1 5 0v0a2.5 2.5 0 0 1-2.5 2.5Z" />
+      <path d="M7 14.5a2.5 2.5 0 0 1-2.5-2.5v0a2.5 2.5 0 0 1 5 0v0a2.5 2.5 0 0 1-2.5 2.5Z" />
     </svg>
   ),
 };
