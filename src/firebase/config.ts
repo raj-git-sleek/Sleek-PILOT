081249ad336f9a14639983cbe092b16050d83ec7
Kh Raj Rahman (Admin - Sleek Apparels) <sleekapparelsltd@gmail.com>
2025-10-23 00:58:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7355003280-c1188",
+  "appId": "1:270305504403:web:1fbbd1e7688671108f7166",
+  "apiKey": "AIzaSyC2rFJf-gChajwsVqC-WIVOxM46Ry9KJ5E",
+  "authDomain": "studio-7355003280-c1188.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "270305504403"
+};
